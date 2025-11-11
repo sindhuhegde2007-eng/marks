41cb5dadbbe0f757527349a13a60f45da9be24f1
sindhu hegde <sindhuhegde2007@gmail.com>
2025-11-11 11:27:18 +0530

    Add default marks functionality to marks.py
    The script now supports default marks if no input is provided and calculates total and average marks accordingly.

--- marks.py
+++ marks.py
@@ -1,8 +1,36 @@
 import sys
-if len(sys.argv) != 6:
-    print("Usage: python average_marks.py mark1 mark2 mark3 mark4 mark5")
-    sys.exit(1)
-marks = [int(sys.argv[i]) for i in range(1, 6)]
-average = sum(marks) / 5
-print("Marks entered:", marks)
-print("Average marks:", average)
+if len(sys.argv) == 6:
+
+    script_name = sys.argv[0]
+    marks1 = (sys.argv[1])
+    marks2 = (sys.argv[2])
+    marks3 = (sys.argv[3])
+    marks4 = (sys.argv[4])
+    marks5 = (sys.argv[5])
+
+else:
+    script_name = sys.argv[0]
+    marks1 = 85
+    marks2 = 90
+    marks3 = 78
+    marks4 = 92
+    marks5 = 88
+    print("Inputs not provided. Using default marks.")
+
+total_marks = int(marks1) + int(marks2) + int(marks3) + int(marks4) + int(marks5)
+average_marks = total_marks / 5
+
+print("Script Name:", script_name)
+print("Total Marks:", total_marks)
+print("Average Marks:", average_marks)
+
+if average_marks >= 90:
+    print("Grade: A")
+elif average_marks >= 80:
+    print("Grade: B")
+elif average_marks >= 70:
+    print("Grade: C")
+elif average_marks >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
